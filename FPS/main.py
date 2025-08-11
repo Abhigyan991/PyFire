06485d6a4f581dec918b5049e736dab35ab3ba15
Abhigyan991 <abhigyan.kar14@gmail.com>
2025-08-11 08:38:47 -0700

    Add files via upload

---
+++ FPS/main.py
@@ -0,0 +1,10 @@
+from engine import App
+
+# Entry point
+if __name__ == "__main__":
+    # Initialize the OpenGL application
+    print("Starting OpenGL application")
+
+    my_app = App()
+    my_app.run()
+    my_app.quit()
